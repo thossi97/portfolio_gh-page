faeb96599ae178d8afeca958add586f243d1bbf9
thossi97 <tho85@hi.is>
2021-03-05 17:30:39 +0000

    laga texta

--- src/components/About.js
+++ src/components/About.js
@@ -25,7 +25,7 @@ class About extends React.Component  {
 Áhugamál mín liggja hvað helst í fótbolta, verja tíma með vinum, tölvuleikjum og að læra og upplifa nýja hluti. 
 Ég hef mikla reynslu af Java og Javascript þar sem þau voru helstu forritunarmálin sem kennd voru í HÍ ásamt þó nokkurri reynslu í Python.
 Í gegnum háskólagöngu mína hafði ég hvað mest gaman af þeim kúrsum sem gengu helst út á að hanna og kóða hugbúnað, hvort sem um ræddi tölvuleiki, vefsíður eða app.
-Ég stefni á áframhaldandi nám einhverntímann í framtíðinni en hugurinn er þó ekki alveg kominn þangað en.
+Ég stefni á áframhaldandi nám einhverntímann í framtíðinni en hugurinn er þó ekki alveg kominn þangað enn.
                     </p>
                 </div>
             </div>
